3d14ad665e7b3f1069ad12b8c62225e1bcda22b0
sirri1222 <hyeonji159753@gmail.com>
2023-02-15 10:56:31 +0900

    커뮤니티에 파이차트 추가

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import Layout from "../common/Layout";
 import CommunityCard from "./CommunityCard";
+import { ResponsivePie, } from "@nivo/pie";
+
 
 // https://quilljs.com/
 // npm install react-quill
@@ -17,6 +19,7 @@ import "react-quill/dist/quill.snow.css";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import axios, { Axios } from "axios";
 
 // 02. form 요소의 항목별 에러 체크 정의
 const schema = yup.object({
@@ -26,6 +29,10 @@ const schema = yup.object({
 });
 
 const Community = () => {
+
+  // html 글자를 화면에 출력하기 위한 샘플
+  // ReactQuill 에서 입력되는 사항을 출력하기 위한 처리
+  const [tag, setTag] = useState("<p><b>Hello</b></p>");
   // 03. useForm 생성
   // register : 각각의 form 의 name 을 설정
   // handleSubmit : form 에서 onSubmit 할때 실행됨
@@ -307,11 +314,165 @@ const Community = () => {
     trigger("contents");
     //----------- useForm
   };
-
+  // 차트 데이터
+  const charData = [
+    {
+      id: "lisp",
+      label: "lisp",
+      value: 511,
+      color: "hsl(174, 70%, 50%)",
+    },
+    {
+      id: "erlang",
+      label: "erlang",
+      value: 272,
+      color: "hsl(357, 70%, 50%)",
+    },
+    {
+      id: "c",
+      label: "c",
+      value: 160,
+      color: "hsl(154, 70%, 50%)",
+    },
+    {
+      id: "ruby",
+      label: "ruby",
+      value: 79,
+      color: "hsl(230, 70%, 50%)",
+    },
+    {
+      id: "javascript",
+      label: "javascript",
+      value: 443,
+      color: "hsl(45, 70%, 50%)",
+    },
+  ];
   return (
     <Layout title={"Community"}>
+       {/* 차트출력 */}
+      {/* 차트출력 */}
+      <div style={{ width: "100%", height: 400 }}>
+        <ResponsivePie
+          data={charData}
+          margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+          innerRadius={0.5}
+          padAngle={0.7}
+          cornerRadius={3}
+          activeOuterRadiusOffset={8}
+          borderWidth={1}
+          borderColor={{
+            from: "color",
+            modifiers: [["darker", 0.2]],
+          }}
+          arcLinkLabelsSkipAngle={10}
+          arcLinkLabelsTextColor="#333333"
+          arcLinkLabelsThickness={2}
+          arcLinkLabelsColor={{ from: "color" }}
+          arcLabelsSkipAngle={10}
+          arcLabelsTextColor={{
+            from: "color",
+            modifiers: [["darker", 2]],
+          }}
+          defs={[
+            {
+              id: "dots",
+              type: "patternDots",
+              background: "inherit",
+              color: "rgba(255, 255, 255, 0.3)",
+              size: 4,
+              padding: 1,
+              stagger: true,
+            },
+            {
+              id: "lines",
+              type: "patternLines",
+              background: "inherit",
+              color: "rgba(255, 255, 255, 0.3)",
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10,
+            },
+          ]}
+          fill={[
+            {
+              match: {
+                id: "ruby",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "c",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "go",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "python",
+              },
+              id: "dots",
+            },
+            {
+              match: {
+                id: "scala",
+              },
+              id: "lines",
+            },
+            {
+              match: {
+                id: "lisp",
+              },
+              id: "lines",
+            },
+            {
+              match: {
+                id: "elixir",
+              },
+              id: "lines",
+            },
+            {
+              match: {
+                id: "javascript",
+              },
+              id: "lines",
+            },
+          ]}
+          legends={[
+            {
+              anchor: "bottom",
+              direction: "row",
+              justify: false,
+              translateX: 0,
+              translateY: 56,
+              itemsSpacing: 0,
+              itemWidth: 100,
+              itemHeight: 18,
+              itemTextColor: "#999",
+              itemDirection: "left-to-right",
+              itemOpacity: 1,
+              symbolSize: 18,
+              symbolShape: "circle",
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemTextColor: "#000",
+                  },
+                },
+              ],
+            },
+          ]}
+        />
+      </div>
       {/* 입력폼 */}
-
+<div>{tag}</div>
+<div dangerouslySetInnerHTML={{__html:tag}}></div>
       <div className="inputBox">
         <form onSubmit={handleSubmit(createPost)}>
           <input
